b4c7d9e61b66e38ed8e6e94467259ca59a36908c
sikiryl <saritrat.206@gmail.com>
2021-10-22 02:11:13 +0700

    feat: upgrade levis to 1.0.16-alpha

--- Formula/levis.rb
+++ Formula/levis.rb
@@ -1,10 +1,10 @@
 class Levis < Formula
   desc "Kubernetes Manifest Generator - CLI"
   homepage "https://github.com/kubeopsskills/levis"
-  url "https://github.com/kubeopsskills/levis/releases/download/1.0.15-alpha/levis-1.0.15-alpha-macos-x64.tar.gz"
-  sha256 "5bd52e9b701d7418dc9d8e8bbefd37a2853c2a6631180bf8324a08ff3de03a29"
+  url "https://github.com/kubeopsskills/levis/releases/download/1.0.16-alpha/levis-1.0.16-alpha-macos-x64.tar.gz"
+  sha256 "5545cfd137696e0a62a9f973af0c8e479373459b350c4b63007733c61de5de2d"
   license "MIT"
-  version "1.0.15-alpha"
+  version "1.0.16-alpha"
 
   def install
     bin.install "levis"
