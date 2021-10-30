31f3bf14b26e253fdd9446b479f3c87e7900cc5e
sikiryl <saritrat.206@gmail.com>
2021-10-30 11:25:19 +0700

    feat: release beta

--- Formula/levis.rb
+++ Formula/levis.rb
@@ -4,7 +4,7 @@ class Levis < Formula
   url "https://github.com/kubeopsskills/levis/releases/download/1.1.0-beta/levis-1.1.0-beta-macos-x64.tar.gz"
   sha256 "5545cfd137696e0a62a9f973af0c8e479373459b350c4b63007733c61de5de2d"
   license "MIT"
-  version "1.0.16-alpha"
+  version "1.1.0-beta"
 
   def install
     bin.install "levis"
