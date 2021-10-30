e48bf620623a04637dfd87a6ccdf4de841c9d6e4
sikiryl <saritrat.206@gmail.com>
2021-10-30 11:41:55 +0700

    feat: release beta

--- Formula/levis.rb
+++ Formula/levis.rb
@@ -2,7 +2,7 @@ class Levis < Formula
   desc "Kubernetes Manifest Generator - CLI"
   homepage "https://github.com/kubeopsskills/levis"
   url "https://github.com/kubeopsskills/levis/releases/download/1.1.0-beta/levis-1.1.0-beta-macos-x64.tar.gz"
-  sha256 "5545cfd137696e0a62a9f973af0c8e479373459b350c4b63007733c61de5de2d"
+  sha256 "f231d8ceb610ad1cd3dfd62abb6a2605d3ef08c01164074c51bc176246ff7463"
   license "MIT"
   version "1.1.0-beta"
 
